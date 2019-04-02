511158bdab9b83143b249e21a6ef2f65f230ba22
tonykevin <hi@tonykevin.com>
2019-04-01 23:05:41 -0500

    http requests: GET, PUT, POST, DELETE

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json('RESTServer')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/user', (req, res) => {
+  res.json('get user')
+})
+
+app.post('/user', (req, res) => {
+  let person = req.body
+  res.json({
+    person
+  })
+})
+
+app.put('/user/:id', (req, res) => {
+  let id = req.params.id
+
+  res.json({
+    id
+  })
+})
+
+app.delete('/user', (req, res) => {
+  res.json('delete  user')
 })
 
 app.listen(PORT, () => console.log(`listening port :${PORT}`))
