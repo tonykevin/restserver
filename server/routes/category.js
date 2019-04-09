4d324d10942cd7f235fd3cb26842e477291a7a33
tonykevin <hi@tonykevin.com>
2019-04-09 00:22:38 -0500

    get list of categories

--- server/routes/category.js
+++ server/routes/category.js
@@ -4,6 +4,34 @@ const { Category } = require('../models')
 
 const app = express()
 
+// List categories
+app.get('/category', verifyToken, (req, res) => {
+  Category.find({})
+    .exec((err, categories) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          err
+        })
+      }
+
+      Category.countDocuments({}, (err, size) => {
+        if (err) {
+          return res.status(500).json({
+            ok: false,
+            err
+          })
+        }
+
+        res.json({
+          ok: true,
+          categories,
+          size
+        })
+      })
+    })
+})
+
 // Create a category
 app.post('/category', verifyToken, (req, res) => {
   let category = new Category({
